bb5abaa5e3e99cca1d5d3790dd68c06ce7332820
Вал <val.shab.ma@gmail.com>
2025-02-03 06:19:19 +0300

    первое выполнение финального задания 6 спринта.

--- src/com/manager/historyManager/InMemoryHistoryManager.java
+++ src/com/manager/historyManager/InMemoryHistoryManager.java
@@ -11,7 +11,7 @@ public class InMemoryHistoryManager implements HistoryManager {
 
    public Task remove(int id) {
        Node remNode = hisMap.get(id);
-       if(remNode != null) {
+        if(remNode != null) {
             removeNode(remNode);
        }
        return null;
