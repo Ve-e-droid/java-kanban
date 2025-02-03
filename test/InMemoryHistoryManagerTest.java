7d106e45644bfb63560142e9e9e187445cb05909
Вал <val.shab.ma@gmail.com>
2025-02-03 06:05:00 +0300

    первое выполнение финального задания 6 спринта.

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -4,9 +4,8 @@ import com.manager.taskManager.InMemoryTaskManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-
 import static org.junit.jupiter.api.Assertions.*;
+
 class InMemoryHistoryManagerTest {
 
     private InMemoryTaskManager taskManager;
