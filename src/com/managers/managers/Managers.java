9709a876fdc8cb3bc507ca98f37e2e65bd8f9fb2
Вал <val.shab.ma@gmail.com>
2025-02-03 06:26:32 +0300

    первое выполнение финального задания 6 спринта.

--- src/com/managers/managers/Managers.java
+++ src/com/managers/managers/Managers.java
@@ -10,6 +10,7 @@ public class Managers {
 
         return new InMemoryTaskManager();
     }
+
     public static HistoryManager getDefaultHistory() {
 
         return new InMemoryHistoryManager();
